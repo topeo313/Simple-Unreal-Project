058f9b96267e113e3935a8581d0c780e80bff1ef
topeo313 <topeo345@gmail.com>
2026-09-19 19:08:31 -0700

    Add LoggerHelper::LogOutput

---
+++ Source/SimpleGame/LoggerHelper.cpp
@@ -0,0 +1,8 @@
+﻿#include "C:\Users\Tope\Documents\Unreal Projects\SimpleGame\Intermediate\Build\Win64\x64\SimpleGameEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "LoggerHelper.h"
+#include "Logging/LogMacros.h"
+
+void LoggerHelper::LogOutput(const FString& message, ELogVerbosity::Type Verbosity)
+{
+	GLog->Log(LogTemp.GetCategoryName(), Verbosity, *message);
+}
---
+++ Source/SimpleGame/LoggerHelper.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <string>
+
+class LoggerHelper
+{
+public:
+	static void LogOutput(const FString& message, ELogVerbosity::Type Verbosity = ELogVerbosity::Warning);
+};
